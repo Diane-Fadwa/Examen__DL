74b6062ae2d5b1c23d42a25ece0b90ed714af700
Diane-Fadwa <133110105+Diane-Fadwa@users.noreply.github.com>
2026-01-15 10:00:46 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,5 +1,5 @@
-Traceback (most recent call last):
+raceback (most recent call last):
   File "<frozen importlib._bootstrap>", line 488, in _call_with_frames_removed
-  File "/opt/airflow/dags/repo/rattrapage/asset_ratt.py", line 3, in <module>
-    from airflow.models.asset import Asset
-ImportError: cannot import name 'Asset' from 'airflow.models.asset' (/home/airflow/.local/lib/python3.12/site-packages/airflow/models/asset.py). Did you mean: 'assets'?
+  File "/opt/airflow/dags/repo/rattrapage/asset_ratt.py", line 4, in <module>
+    from airflow.decorators import get_current_context
+ImportError: cannot import name 'get_current_context' from 'airflow.decorators' (/home/airflow/.local/lib/python3.12/site-packages/airflow/decorators/__init__.py)
