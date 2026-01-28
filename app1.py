582f1a201335aedea373f7917d87fcc541071424
Diane-Fadwa <133110105+Diane-Fadwa@users.noreply.github.com>
2026-01-28 14:56:51 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,165 +1,201 @@
-[cdprct@cdp-gateway-01-rct ~]$ hdfs dfs -ls hdfs://nameservice1/raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250501.txt
--rw-r--r--   1 cloudera-scm supergroup    3530244 2026-01-16 16:08 hdfs://nameservice1/raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250501.txt
-[cdprct@cdp-gateway-01-rct ~]$ hdfs dfs -cp hdfs://nameservice1/raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250501.txt hdfs://nameservice1/raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt
-[cdprct@cdp-gateway-01-rct ~]$ cd /opt/workspace/Script/CEKO/hcomp/lib/bin
-[cdprct@cdp-gateway-01-rct bin]$ ./hcompressor
+import logging
+import random
+from datetime import datetime
+from pathlib import Path
 
-usage: hcompressor [-h] [--skip_extension SKIP_EXTENSION]
-                   [--only_extension ONLY_EXTENSION] [--trust {0,1,2}]
-                   [--logs_file LOGS_FILE] [--silent [SILENT]]
-                   [--compression_type {bz2,zstd,gz}] [-r [RECURSIVE]]
-                   [--mode {compression,decompression}]
-                   [--delete_input [DELETE_INPUT]]
-                   input_path output_path
-hcompressor: error: the following arguments are required: input_path, output_path
-[cdprct@cdp-gateway-01-rct bin]$ ./hcompressor ^C
-[cdprct@cdp-gateway-01-rct bin]$ ./hcompressor hdfs://nameservice1/raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt ^C
-[cdprct@cdp-gateway-01-rct bin]$ ./hcompressor hdfs://nameservice1/raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt  hdfs://nameservice1/raw/ebk_web_device_history/16-Jan-2026
+from airflow import DAG
+from airflow.decorators import task, task_group
+from airflow.sdk import Asset, get_current_context
+from airflow.providers.ssh.hooks.ssh import SSHHook
 
-3314085 | 2026-01-28 13:35:02,066 | hcompressor | INFO | Arguments parsed successfully
-3314085 | 2026-01-28 13:35:13,936 | hcompressor | INFO | Files to compress : 1
-3314085 | 2026-01-28 13:35:13,937 | hcompressor | INFO | Compressing hdfs://nameservice1/raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt to hdfs://nameservice1/raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt.zstd
--put: java.net.URISyntaxException: Expected scheme-specific part at index 5: hdfs:
-Usage: hadoop fs [generic options]
-        [-appendToFile <localsrc> ... <dst>]
-        [-cat [-ignoreCrc] <src> ...]
-        [-checksum <src> ...]
-        [-chgrp [-R] GROUP PATH...]
-        [-chmod [-R] <MODE[,MODE]... | OCTALMODE> PATH...]
-        [-chown [-R] [OWNER][:[GROUP]] PATH...]
-        [-copyFromLocal [-f] [-p] [-l] [-d] [-t <thread count>] <localsrc> ... <dst>]
-        [-copyToLocal [-f] [-p] [-ignoreCrc] [-crc] <src> ... <localdst>]
-        [-count [-q] [-h] [-v] [-t [<storage type>]] [-u] [-x] [-e] [-s] <path> ...]
-        [-cp [-f] [-p | -p[topax]] [-d] <src> ... <dst>]
-        [-createSnapshot <snapshotDir> [<snapshotName>]]
-        [-deleteSnapshot <snapshotDir> <snapshotName>]
-        [-df [-h] [<path> ...]]
-        [-du [-s] [-h] [-v] [-x] <path> ...]
-        [-expunge [-immediate]]
-        [-find <path> ... <expression> ...]
-        [-get [-f] [-p] [-ignoreCrc] [-crc] <src> ... <localdst>]
-        [-getfacl [-R] <path>]
--cat: java.net.URISyntaxException: Expected scheme-specific part at index 5: hdfs:
-Usage: hadoop fs [generic options]
-        [-appendToFile <localsrc> ... <dst>]
-        [-cat [-ignoreCrc] <src> ...]
-        [-checksum <src> ...]
-        [-getfattr [-R] {-n name | -d} [-e en] <path>]
-        [-chgrp [-R] GROUP PATH...]
-        [-getmerge [-nl] [-skip-empty-file] <src> <localdst>]   [-chmod [-R] <MODE[,MODE]... | OCTALMODE> PATH...]
-        [-chown [-R] [OWNER][:[GROUP]] PATH...]
+from awb_lib.providers.knox.hooks.knox_livy_hook import KnoxLivyHook
 
-        [-copyFromLocal [-f] [-p] [-l] [-d] [-t <thread count>] <localsrc> ... <dst>]
-        [-head <file>]  [-copyToLocal [-f] [-p] [-ignoreCrc] [-crc] <src> ... <localdst>]
+logger = logging.getLogger(__name__)
 
-        [-help [cmd ...]]
-        [-count [-q] [-h] [-v] [-t [<storage type>]] [-u] [-x] [-e] [-s] <path> ...]
-        [-cp [-f] [-p | -p[topax]] [-d] <src> ... <dst>]
-        [-createSnapshot <snapshotDir> [<snapshotName>]]
-        [-ls [-C] [-d] [-h] [-q] [-R] [-t] [-S] [-r] [-u] [-e] [<path> ...]]
-        [-deleteSnapshot <snapshotDir> <snapshotName>]
-        [-df [-h] [<path> ...]]
-        [-mkdir [-p] <path> ...]        [-du [-s] [-h] [-v] [-x] <path> ...]
+# Asset consommé
 
-        [-expunge [-immediate]] [-moveFromLocal [-f] [-p] [-l] [-d] <localsrc> ... <dst>]
+asset_rattrapage = Asset("replay://rattrapage")
 
-        [-moveToLocal <src> <localdst>]
-        [-mv <src> ... <dst>]
-        [-put [-f] [-p] [-l] [-d] [-t <thread count>] <localsrc> ... <dst>]
-        [-renameSnapshot <snapshotDir> <oldName> <newName>]
-        [-find <path> ... <expression> ...]
-        [-rm [-f] [-r|-R] [-skipTrash] [-safely] <src> ...]
-        [-get [-f] [-p] [-ignoreCrc] [-crc] <src> ... <localdst>]
-        [-rmdir [--ignore-fail-on-non-empty] <dir> ...]
-        [-getfacl [-R] <path>]
-        [-setfacl [-R] [{-b|-k} {-m|-x <acl_spec>} <path>]|[--set <acl_spec> <path>]]
-        [-setfattr {-n name [-v value] | -x name} <path>]
-        [-setrep [-R] [-w] <rep> <path> ...]
-        [-stat [format] <path> ...]
-        [-tail [-f] [-s <sleep interval>] <file>]
-        [-test -[defsz] <path>]
-        [-text [-ignoreCrc] <src> ...]
-        [-touch [-a] [-m] [-t TIMESTAMP (yyyyMMdd:HHmmss) ] [-c] <path> ...]
-        [-touchz <path> ...]
-        [-truncate [-w] <length> <path> ...]
-        [-usage [cmd ...]]
+# Validation + explosion du JSON porté par l’AssetEvent
 
-Generic options supported are:
--conf <configuration file>        specify an application configuration file
--D <property=value>               define a value for a given property
--fs <file:///|hdfs://namenode:port> specify default filesystem URL to use, overrides 'fs.defaultFS' property from configurations.
--jt <local|resourcemanager:port>  specify a ResourceManager     [-getfattr [-R] {-n name | -d} [-e en] <path>]
--files <file1,...>                specify a comma-separated list of files to be copied to the map reduce cluster
+@task
+def validate_rattrapage_payload():
+    ctx = get_current_context()
 
--libjars <jar1,...>               specify a comma-separated list of jar files to be included in the classpath
--archives <archive1,...>          specify a comma-separated list of archives to be unarchived on the compute machines
+    events = ctx.get("triggering_asset_events")
+    if not events:
+        raise ValueError("No triggering asset events found")
 
-The general command line syntax is:
-command [genericOptions] [commandOptions]
+    asset_events = events.get(asset_rattrapage)
+    if not asset_events:
+        raise ValueError("No events found for asset replay://rattrapage")
 
-Usage: hadoop fs [generic options] -put [-f] [-p] [-l] [-d] [-t <thread count>] <localsrc> ... <dst>
-        [-getmerge [-nl] [-skip-empty-file] <src> <localdst>]
-        [-head <file>]
-        [-help [cmd ...]]
-        [-ls [-C] [-d] [-h] [-q] [-R] [-t] [-S] [-r] [-u] [-e] [<path> ...]]
-        [-mkdir [-p] <path> ...]
-        [-moveFromLocal [-f] [-p] [-l] [-d] <localsrc> ... <dst>]
-        [-moveToLocal <src> <localdst>]
-        [-mv <src> ... <dst>]
-        [-put [-f] [-p] [-l] [-d] [-t <thread count>] <localsrc> ... <dst>]
-        [-renameSnapshot <snapshotDir> <oldName> <newName>]
-        [-rm [-f] [-r|-R] [-skipTrash] [-safely] <src> ...]
-        [-rmdir [--ignore-fail-on-non-empty] <dir> ...]
-        [-setfacl [-R] [{-b|-k} {-m|-x <acl_spec>} <path>]|[--set <acl_spec> <path>]]
-        [-setfattr {-n name [-v value] | -x name} <path>]
-        [-setrep [-R] [-w] <rep> <path> ...]
-        [-stat [format] <path> ...]
-        [-tail [-f] [-s <sleep interval>] <file>]
-        [-test -[defsz] <path>]
-        [-text [-ignoreCrc] <src> ...]
-        [-touch [-a] [-m] [-t TIMESTAMP (yyyyMMdd:HHmmss) ] [-c] <path> ...]
-        [-touchz <path> ...]
-        [-truncate [-w] <length> <path> ...]
-        [-usage [cmd ...]]
+    payload = asset_events[-1].extra
+    logger.info("Payload reçu depuis l'Asset : %s", payload)
 
-Generic options supported are:
--conf <configuration file>        specify an application configuration file
--D <property=value>               define a value for a given property
--fs <file:///|hdfs://namenode:port> specify default filesystem URL to use, overrides 'fs.defaultFS' property from configurations.
--jt <local|resourcemanager:port>  specify a ResourceManager
--files <file1,...>                specify a comma-separated list of files to be copied to the map reduce cluster
--libjars <jar1,...>               specify a comma-separated list of jar files to be included in the classpath
--archives <archive1,...>          specify a comma-separated list of archives to be unarchived on the compute machines
+    if not isinstance(payload, dict):
+        raise ValueError("Asset payload must be a JSON object")
 
-The general command line syntax is:
-command [genericOptions] [commandOptions]
+    if "contract_path" not in payload:
+        raise ValueError("Missing 'contract_path'")
 
-Usage: hadoop fs [generic options] -cat [-ignoreCrc] <src> ...
-3314085 | 2026-01-28 13:35:22,014 | hcompressor | ERROR | Error while compression files: [Errno 32] Broken pipe
-Traceback (most recent call last):
-  File "./hcompressor", line 22, in <module>
-    hcompressor.hcompressor.main()
-  File "/opt/workspace/Script/CEKO/hcomp/lib/hcompressor/hcompressor.py", line 91, in main
-    raise e
-  File "/opt/workspace/Script/CEKO/hcomp/lib/hcompressor/hcompressor.py", line 84, in main
-    compressor_writer.compress_write()
-  File "/opt/workspace/Script/CEKO/hcomp/lib/hcompressor/compressors.py", line 121, in compress_write
-    self.run_file_compression(chunk_size, input_file_path)
-  File "/opt/workspace/Script/CEKO/hcomp/lib/hcompressor/compressors.py", line 134, in run_file_compression
-    chunk_size=chunk_size,
-  File "/opt/workspace/Script/CEKO/hcomp/lib/hcompressor/compressors.py", line 353, in compress_file_write
-    file_out.write(chunk)
-  File "/opt/workspace/Script/CEKO/hcomp/env/lib/python3.7/site-packages/smart_open/hdfs.py", line 139, in close
-    self.flush()
-  File "/opt/workspace/Script/CEKO/hcomp/env/lib/python3.7/site-packages/smart_open/hdfs.py", line 144, in flush
-    self._sub.stdin.flush()
-BrokenPipeError: [Errno 32] Broken pipe
-[cdprct@cdp-gateway-01-rct bin]$
-[cdprct@cdp-gateway-01-rct bin]$
-[cdprct@cdp-gateway-01-rct bin]$
-[cdprct@cdp-gateway-01-rct bin]$ ./hcompressor /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt  /raw/ebk_web_device_history/16-Jan-2026
+    if "files" not in payload:
+        raise ValueError("Missing 'files'")
 
-3315040 | 2026-01-28 13:36:14,359 | hcompressor | INFO | Arguments parsed successfully
-3315040 | 2026-01-28 13:36:26,260 | hcompressor | INFO | Files to compress : 1
-3315040 | 2026-01-28 13:36:26,260 | hcompressor | INFO | Compressing /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt to /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt.zstd
-3315040 | 2026-01-28 13:36:35,315 | hcompressor | INFO | Time : 9.055138 seconds, compression type: zstd
+    contract_path = payload["contract_path"]
+    files = payload["files"]
+
+    if not isinstance(contract_path, str) or not contract_path.startswith("hdfs://"):
+        raise ValueError("'contract_path' must be an HDFS path")
+
+    if not isinstance(files, list) or not files:
+        raise ValueError("'files' must be a non-empty list")
+
+    for f in files:
+        if not isinstance(f, str) or not f.startswith("hdfs://"):
+            raise ValueError(f"Invalid HDFS file path: {f}")
+
+    logger.info(
+        "Rattrapage validé : contract=%s | %d fichiers",
+        contract_path,
+        len(files),
+    )
+
+    # Explosion : 1 fichier = 1 exécution
+    return [
+        {
+            "contract_path": contract_path,
+            "file_path": f,
+        }
+        for f in files
+    ]
+
+# DAG déclenché UNIQUEMENT par l’Asset
+
+with DAG(
+    dag_id="dag_rattrapage",
+    description="DAG de rattrapage déclenché par l’Asset replay://rattrapage",
+    start_date=datetime(2026, 1, 1),
+    schedule=[asset_rattrapage],
+    catchup=False,
+    default_args={"owner": "airflow", "retries": 1},
+    tags=["rattrapage", "asset", "replay"],
+) as dag:
+
+    files_to_process = validate_rattrapage_payload()
+
+    # Traitement par fichier
+
+    @task_group
+    def process_file(contract_path: str, file_path: str):
+
+        # Décompression si nécessaire (.zstd uniquement)
+
+        @task
+        def decompress_if_needed(file_path: str) -> str:
+            # Fichier non compressé → passage direct
+            if not file_path.endswith(".zstd"):
+                logger.info("Fichier non compressé : %s", file_path)
+                return file_path
+
+            ssh_hook = SSHHook(ssh_conn_id="SSH_REC")
+            ssh_client = ssh_hook.get_conn()
+
+            # HDFS → path machine
+
+            machine_input_path = file_path.replace("hdfs://nameservice1", "")
+            machine_output_path = machine_input_path.replace(".zstd", "")
+
+            cmd = f"""
+            cd /opt/workspace/Script/CEKO/hcomp/lib/bin && \
+            ./hcompressor \
+              --mode decompression \
+              --compression_type zstd \
+              --delete_input 0 \
+              {machine_input_path} {machine_output_path}
+            """
+
+            logger.info("Décompression via SSH : %s", machine_input_path)
+
+            exit_code, stdout, stderr = ssh_hook.exec_ssh_client_command(
+                ssh_client=ssh_client,
+                command=cmd,
+                get_pty=True,
+                environment=None,
+            )
+
+            if exit_code != 0:
+                raise RuntimeError(
+                    f"Erreur hcompressor (exit={exit_code})\n"
+                    f"stdout={stdout.decode()}\n"
+                    f"stderr={stderr.decode()}"
+                )
+
+            # Reconstruction du path HDFS pour Spark
+            hdfs_output_path = "hdfs://nameservice1" + machine_output_path
+
+            logger.info("Fichier décompressé prêt pour Spark : %s", hdfs_output_path)
+            return hdfs_output_path
+
+        # Spark validation + ingestion
+
+        @task
+        def spark_validate_ingest(contract_path: str, file_path: str):
+            livy_hook = KnoxLivyHook(conn_id="KNOX_REC")
+
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            random_suffix = random.randint(1000, 9999)
+
+            job_name = (
+                f"rattrapage_{Path(file_path).stem}_{timestamp}_{random_suffix}"
+            )
+
+            logger.info("Submitting Spark job %s", job_name)
+            logger.info("Contract: %s", contract_path)
+            logger.info("Input file: %s", file_path)
+
+            batch_id = livy_hook.post_batch(
+                file="hdfs://nameservice1/awb_rec/awb_ingestion/artifacts/"
+                     "ebk_web_device_history/check_meta_from_contract.py",
+                name=job_name,
+                args=[
+                    contract_path,
+                    file_path,
+                ],
+                queue="default",
+                conf={
+                    "spark.sql.sources.partitionOverwriteMode": "dynamic",
+                    "spark.sql.adaptive.enabled": "true",
+                    "spark.dynamicAllocation.enabled": "true",
+                },
+                driver_memory="1g",
+                driver_cores=1,
+                executor_memory="2g",
+                executor_cores=2,
+                num_executors=2,
+            )
+
+            final_state = livy_hook.poll_for_completion(
+                session_id=batch_id,
+                polling_interval=30,
+                max_polling_attempts=120,
+            )
+
+            logger.info(
+                "Spark job %s terminé avec l’état %s",
+                batch_id,
+                final_state.value,
+            )
+
+            return {
+                "batch_id": batch_id,
+                "file": file_path,
+                "state": final_state.value,
+            }
+
+        # Chaînage logique
+        decompressed_file = decompress_if_needed(file_path)
+        spark_validate_ingest(contract_path, decompressed_file)
+
+    # Mapping dynamique
+    process_file.expand_kwargs(files_to_process)
