b48f84e81c356e6d241f6bafb9b62ebf2a1a759d
Diane-Fadwa <133110105+Diane-Fadwa@users.noreply.github.com>
2026-01-29 12:38:52 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,29 +1,35 @@
+[2026-01-29, 12:38:07] WARNING - /home/airflow/.local/lib/python3.12/site-packages/airflow/models/connection.py:471: DeprecationWarning: Using Connection.get_connection_from_secrets from `airflow.models` is deprecated.Please use `get` on Connection from sdk(`airflow.sdk.Connection`) instead
+  warnings.warn(
+: source="py.warnings"
+[2026-01-29, 12:38:07] INFO - Connection Retrieved 'SSH_REC': source="airflow.hooks.base"
+[2026-01-29, 12:38:07] WARNING - No Host Key Verification. This won't protect against Man-In-The-Middle attacks: source="airflow.task.hooks.airflow.providers.ssh.hooks.ssh.SSHHook"
+[2026-01-29, 12:38:07] INFO - Connected (version 2.0, client OpenSSH_8.7): source="paramiko.transport"
+[2026-01-29, 12:38:07] INFO - Auth banner: b'---------------------------!! ATTENTION !!-----------------------------------\n\nAttention : vous etes maintenant connectes a un systeme informatique securise\n\nde ATTIJARIWAFA BANK. Toutes les actions menees sur ce systeme sont monitorees.\n\n-----------------------------------------------------------------------------\n': source="paramiko.transport"
+[2026-01-29, 12:38:07] INFO - Authentication (password) successful!: source="paramiko.transport"
+[2026-01-29, 12:38:07] INFO - Décompression via hcompressor : /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.zstd: source="unusual_prefix_eb329238963d72514d1fc4fc57b48c5803517fb6_dag_rattrapage"
+[2026-01-29, 12:38:07] INFO - Running command: 
+            cd /opt/workspace/Script/CEKO/hcomp/lib/bin &&             ./hcompressor               --mode decompression               --compression_type zstd               --delete_input 0               /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.zstd /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502
+            : source="airflow.task.hooks.airflow.providers.ssh.hooks.ssh.SSHHook"
+[2026-01-29, 12:38:07] INFO - : source="airflow.task.hooks.airflow.providers.ssh.hooks.ssh.SSHHook"
+[2026-01-29, 12:38:07] INFO - 3917236 | 2026-01-29 12:38:07,428 | hcompressor | INFO | Arguments parsed successfully: source="airflow.task.hooks.airflow.providers.ssh.hooks.ssh.SSHHook"
+[2026-01-29, 12:38:17] ERROR - Task failed with exception: source="task"
+AirflowException: SSH command timed out
+File "/home/airflow/.local/lib/python3.12/site-packages/airflow/sdk/execution_time/task_runner.py", line 920 in run
 
-[cdprct@cdp-gateway-01-rct bin]$ ./hcompressor \
-  --mode decompression \
-  --compression_type zstd \
-  --delete_input 0 \
-  /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt.zstd \
-  /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt
+File "/home/airflow/.local/lib/python3.12/site-packages/airflow/sdk/execution_time/task_runner.py", line 1215 in _execute_task
 
-3890089 | 2026-01-29 11:55:59,971 | hcompressor | INFO | Arguments parsed successfully
-3890089 | 2026-01-29 11:56:11,902 | hcompressor | INFO | 1 files to uncompress
-3890089 | 2026-01-29 11:56:11,902 | hcompressor | INFO | Decompressing /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt.zstd to /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt/ebk_web_device_history_20250502.txt
-put: Current inode is not a directory: ebk_web_device_history_20250502.txt(179596804, INodeFile@21d21df5, parentDir=16-Jan-2026/)
-3890089 | 2026-01-29 11:56:20,292 | hcompressor | ERROR | Error while decompression files: [Errno 32] Broken pipe
-Traceback (most recent call last):
-  File "./hcompressor", line 22, in <module>
-    hcompressor.hcompressor.main()
-  File "/opt/workspace/Script/CEKO/hcomp/lib/hcompressor/hcompressor.py", line 91, in main
-    raise e
-  File "/opt/workspace/Script/CEKO/hcomp/lib/hcompressor/hcompressor.py", line 87, in main
-    compressor_writer.decompress_write()
-  File "/opt/workspace/Script/CEKO/hcomp/lib/hcompressor/compressors.py", line 204, in decompress_write
-    self.run_file_decompression(chunk_size, input_file_path)
-  File "/opt/workspace/Script/CEKO/hcomp/lib/hcompressor/compressors.py", line 218, in run_file_decompression
-    chunk_size=chunk_size,
-  File "/opt/workspace/Script/CEKO/hcomp/lib/hcompressor/compressors.py", line 371, in decompress_file_write
-    file_out.write(chunk)
-  File "/opt/workspace/Script/CEKO/hcomp/env/lib/python3.7/site-packages/smart_open/hdfs.py", line 155, in write
-    self._sub.stdin.write(b)
-BrokenPipeError: [Errno 32] Broken pipe
+File "/home/airflow/.local/lib/python3.12/site-packages/airflow/sdk/bases/operator.py", line 397 in wrapper
+
+File "/home/airflow/.local/lib/python3.12/site-packages/airflow/sdk/bases/decorator.py", line 251 in execute
+
+File "/home/airflow/.local/lib/python3.12/site-packages/airflow/sdk/bases/operator.py", line 397 in wrapper
+
+File "/home/airflow/.local/lib/python3.12/site-packages/airflow/providers/standard/operators/python.py", line 216 in execute
+
+File "/home/airflow/.local/lib/python3.12/site-packages/airflow/providers/standard/operators/python.py", line 239 in execute_callable
+
+File "/home/airflow/.local/lib/python3.12/site-packages/airflow/sdk/execution_time/callback_runner.py", line 81 in run
+
+File "/opt/airflow/dags/repo/rattrapage/dag_rattrapage.py", line 117 in decompress_if_needed
+
+File "/home/airflow/.local/lib/python3.12/site-packages/airflow/providers/ssh/hooks/ssh.py", line 503 in exec_ssh_client_command
