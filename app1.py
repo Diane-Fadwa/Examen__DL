721fe4f0b9bdad53d818f78d02cb47d929c250ad
Diane-Fadwa <133110105+Diane-Fadwa@users.noreply.github.com>
2026-01-29 11:49:55 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,14 +1,6 @@
 
-[cdprct@cdp-gateway-01-rct bin]$ hdfs dfs -ls /raw/ebk_web_device_history
-Found 11 items
-drwxr-xr-x   - cloudera-scm supergroup          0 2026-01-02 18:24 /raw/ebk_web_device_history/02-Jan-2026
-drwxr-xr-x   - cloudera-scm supergroup          0 2026-01-06 19:40 /raw/ebk_web_device_history/06-Jan-2026
-drwxr-xr-x   - cloudera-scm supergroup          0 2026-01-08 14:07 /raw/ebk_web_device_history/08-Jan-2026
-drwxr-xr-x   - cloudera-scm supergroup          0 2026-01-12 19:05 /raw/ebk_web_device_history/12-Jan-2026
-drwxr-xr-x   - cloudera-scm supergroup          0 2026-01-15 19:41 /raw/ebk_web_device_history/15-Jan-2026
-drwxr-xr-x   - cloudera-scm supergroup          0 2026-01-28 13:36 /raw/ebk_web_device_history/16-Jan-2026
-drwxr-xr-x   - cloudera-scm supergroup          0 2026-01-27 16:35 /raw/ebk_web_device_history/17-Dec-2025
-drwxr-xr-x   - cloudera-scm supergroup          0 2025-12-18 17:00 /raw/ebk_web_device_history/18-Dec-2025
-drwxr-xr-x   - cloudera-scm supergroup          0 2025-12-12 17:57 /raw/ebk_web_device_history/2025-12-12
-drwxr-xr-x   - cloudera-scm supergroup          0 2025-12-15 10:49 /raw/ebk_web_device_history/2025-12-15
-drwxr-xr-x   - cloudera-scm supergroup          0 2026-01-28 11:27 /raw/ebk_web_device_history/28-Jan-2026
+[cdprct@cdp-gateway-01-rct bin]$ hdfs dfs -ls /raw/ebk_web_device_history/16-Jan-2026
+Found 3 items
+-rw-r--r--   1 cloudera-scm supergroup    3530244 2026-01-16 16:08 /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250501.txt
+-rw-r--r--   1 cloudera-scm supergroup    3530244 2026-01-28 13:32 /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt
+-rw-r--r--   1 cloudera-scm supergroup     319642 2026-01-28 13:36 /raw/ebk_web_device_history/16-Jan-2026/ebk_web_device_history_20250502.txt.zstd
